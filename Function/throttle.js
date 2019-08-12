d720ea9b5a78b378919a4a6b4ffee7629e140180
Vitaly Kuklin <vitalykuklin@gmail.com>
2019-08-12 23:49:49 +0700

    add throttle

---
+++ Function/throttle.js
@@ -0,0 +1,23 @@
+function throttle(func, ms) {
+// debugger
+    let isThrottled = false,
+        savedArgs,
+        savedThis;
+    function wrapper() {
+        if (isThrottled) { // (2)
+            savedArgs = arguments;
+            savedThis = this;
+            return;
+        }
+        func.apply(this, arguments); // (1)
+        isThrottled = true;
+        setTimeout(function () {
+            isThrottled = false; // (3)
+            if (savedArgs) {
+                wrapper.apply(savedThis, savedArgs);
+                savedArgs = savedThis = null;
+            }
+        }, ms);
+    }
+    return wrapper;
+}
