69f9e251eeb7267fddb53712a527c516cf1ec0c6
kuklinv <vitalykuklin@gmail.com>
2019-08-13 17:28:46 +0700

    mod spy end

--- Function/spyCall.js
+++ Function/spyCall.js
@@ -1,19 +1,17 @@
 function f(a, b) {
-    console.log( a + b ); // произвольная функция или метод
+    console.log(a + b); // произвольная функция или метод
 }
 work = spy(f);
-function spy (f) {
-    function wrapper (...arg) {
-        wrapper.calls.push(arg);
-        return  f.apply(this, arguments);
+function spy(f) {
+    f.calls = [];
+    function wrapper(...arg) {
+        f.calls.push(arg);
+        return f.apply(this, arguments);
     }
-    wrapper.calls = [];
     return wrapper;
 }
-
 work(1, 2); // 3
 work(4, 5); // 9
-
-for (let args of work.calls) {
-    console.log( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+for (let args of f.calls) {
+    console.log('call:' + args.join()); // "call:1,2", "call:4,5"
 }
